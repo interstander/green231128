338b3d225ed519a665fb9ad46b6b2ff6d5429555
interstander <33653240+interstander@users.noreply.github.com>
2024-02-06 16:26:46 +0900

    24년 2월 6일 - 오라클 - 문제풀이
    24년 2월 6일 - 오라클 - 문제풀이
    
    오라클 - 그룹함수,데이터조작 문제 풀이(서브쿼리가 필요한 부분 제외

--- oracle/문제풀이.sql
+++ oracle/문제풀이.sql
@@ -254,13 +254,153 @@ ALTER TABLE dept
     DROP UNUSED COLUMNS;
 DESC dept;
 
+---------------------------------------------------------------------------------
+-- 그룹함수
+-- 1. 모든 사원의 급여 최고액, 최저액, 총액 및 평균급여를 출력하세요. 
+-- 칼럼의 명칭은 최고액(Maximun) 최저액(Minimun), 총액(Sum), 평균 급여(Average)로 지정하고 
+-- 평균에 대해서는 정수로 반올림 하세요.
+SELECT
+    MAX(salary) AS "Maximun",
+    MIN(salary) AS "Minimun",
+    SUM(salary) AS "Sum",
+    ROUND(AVG(salary)) AS "Average"
+FROM employee;
 
 
 
+-- 2. 각 담당업무 유형별로 급여 최고액, 최저액, 총액 및 평균액을 출력하세요. 
+-- 칼럼의 명칭은 최고액(Maximun) 최저액(Minimun), 총액(Sum), 평균 급여(Average)로 지정하고 
+-- 평균에 대해서는 정수로 반올림 하세요.
+SELECT job AS "담당업무",
+    MAX(salary) AS "Maximun",
+    MIN(salary) AS "Minimun",
+    SUM(salary) AS "Sum",
+    ROUND(AVG(salary)) AS "Average"
+FROM employee
+GROUP BY job;
+
+-- 3. Count(*)함수를 이용해서 담당업무가 동일한 사원의 수를 출력하세요
+SELECT job, COUNT(*)
+FROM employee
+GROUP BY job;
+
+
+-- 4. 관리자의수를 나열하세요. 칼럼의 별칭은 COUNT(MANAGER)로 출력하세요
+SELECT COUNT(DISTINCT manager) AS "COUNT(MANAGER)"
+FROM employee;
+
+
+-- 5. 급여 최고액, 급여 최저액의 차액을 출력하세요.
+SELECT MAX(salary)-MIN(salary) AS "최대 최소 차액"
+FROM employee;
 
 
 
+-- 6. 직급별 사원의 최저 급여를 출력하세요. 
+-- 관리자를 알 수 없는 사원 및 최저 급여가 2000미만인 그룹은 제외시키고 
+-- 급여에 대한 내림차순으로 정렬하여 출력하세요
+SELECT job, MIN(salary)
+FROM employee
+WHERE manager IS NOT null
+GROUP BY job
+HAVING NOT (MIN(salary) < 2000)
+ORDER BY MIN(salary) DESC;
 
+
+
+-- 7. 각 부서에 대해 부서번호, 사원수, 부서내의 모든 사원의 평균급여를 출력하시오, 
+-- 칼럼의 별칭은 부서번호(DNO), 사원수(Number of PeoPle), 평균급여(Salary)로 지정하고 
+-- 평균 급여는 소수점2째자리에서 반올림 하세요.
+SELECT dno, 
+       COUNT(*) AS "Number of PeoPle", 
+       ROUND(AVG(salary),1) AS "Salary"
+FROM employee
+GROUP BY dno;
+
+
+
+-- 8. 각 부서에 대해 부서번호 부서이름, 지역명, 사원수, 부서내의 모든 사원의 평균 급여를 출력하시오. 
+-- 칼럼의 별칭은 부서번호 이름(DName), 지역명(Location), 사원수(Number of PeoPle), 평균급여(Salary)로 지정하고 
+-- 평균급여는 정수로 반올림하세요.
+SELECT * FROM employee;
+SELECT * FROM department;
+
+SELECT dno,
+       DECODE(dno,10,'ACCOUNTING',
+                  20,'RESEARCH',
+                  30,'SALES',
+                  40,'OPERATIONS',
+                  NULL) AS "DName",
+        DECODE(dno,10,'NEW YORK',
+                   20,'DALLAS',
+                   30,'CHICAGO',
+                   40,'BOSTON',
+                   NULL) AS "Location",
+        COUNT(*) AS "Number of PeoPle",
+        ROUND(AVG(salary)) AS "Salary"
+FROM employee
+GROUP BY dno;
+
+
+-- 9. 업무를 표시한 다음 해당 업무에 대해 부서번호별 급여 및 부서10,20,30의 급여 총액을 각각 출력하시오. 
+-- 각 칼럼의 별칭은 각각 job, 부서10,부서20, 부서30, 총액으로 지정하세요.
+SELECT dno,job,
+        DECODE(dno,10,SUM(salary)) AS "부서10",
+        DECODE(dno,20,SUM(salary)) AS "부서20",
+        DECODE(dno,30,SUM(salary)) AS "부서30",
+        SUM(salary) AS "급여 총액"
+FROM employee
+GROUP BY dno, job
+ORDER BY dno, job;
+
+---------------------------------------------------------------------------------
+-- 데이터 조작
+--1. Employee 테이블의 구조만 복사하여 EMP_INSERT란 빈 테이블을 만드세요.
+CREATE TABLE emp_insert
+AS SELECT * FROM employee WHERE 1=0;
+
+DESC emp_insert;
+SELECT * FROM emp_insert;
+
+--2. 본인을 EMP_INSERT 테이블에 추가하되 SYSDATE를 이용해서 입사일을 오늘로 입력하세요
+INSERT INTO emp_insert
+VALUES(1,'홍길동','teacher',null,sysdate,1000,250,10);
+
+SELECT * FROM emp_insert;
+
+--3. EMP_INSERT 테이블에 옆사람을 추가하되 TO_DATE함수를 사용해서 입사일을 어제로 입력하세요
+INSERT INTO emp_insert
+VALUES(2,'박길동','Student',null,TO_DATE(sysdate-1,'YYYY/MM/DD'),800,100,10);
+
+--4. Employee 테이블의 구조와 내용을 복사하여 EMP_COPY란 이름의 테이블을 만드세요.
+CREATE TABLE emp_copy
+AS SELECT * FROM employee;
+
+DESC emp_copy;
+SELECT * FROM emp_copy;
+
+--5. 사원번호가 7788인 사원의 부서번호를 10으로 수정하세요.
+UPDATE emp_copy 
+SET dno = 10 
+WHERE eno=7788; 
+
+--6. 사원번호가 7788의 담당업무및 급여를 사원번호 7499의 담당 업무및 급여와 일치하도록 갱신하세요.(=> 서브 쿼리)
+
+--7. 사원번호 7369와 업무가 동일한 모든 사원의 부서번호를 사원7369의 현재 부서번호로 갱신하세요.(=> 서브 쿼리)
+
+--8. Department 테이블의 구조와 내용을 복사하여 DEPT_COPY란 이름의 테이블을 만드세요
+CREATE TABLE dept_copy
+AS SELECT * FROM department;
+
+DESC dept_copy;
+SELECT * FROM dept_copy;
+
+--9. DEPT_COPY 테이블에서 부서명이 RESEARCH인 부서를 제거하세요.
+DELETE FROM dept_copy WHERE dname='RESEARCH';
+
+--10. DEPT_COPY 테이블에서 부서번호가 10이거나 40인 부서를 제거하세요
+DELETE FROM dept_copy WHERE dno IN (10, 40);
+DELETE FROM dept_copy WHERE dno=10 OR dno=40;
 
 
 
